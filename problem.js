8375ba35f84a462e82f34729e2253ec35c50418a
rayhan <mohammadrayhanx0@gmail.com>
2025-08-18 01:30:05 +0600

    problem 3 added

--- problem.js
+++ problem.js
@@ -15,19 +15,37 @@
 // Problem 2: Count Vowels in a String
 // Write a function that counts how many vowels (a, e, i, o, u) are in a given string.
 
-function vowelCounter(str){
-    const vowel ="aeiouAEIOU";
-    let count = 0 ;
+// function vowelCounter(str){
+//     const vowel ="aeiouAEIOU";
+//     let count = 0 ;
 
-    for(let char of str ){
-        // console.log(char)
-        if(vowel.includes(char)){
-            count ++;
-        }
+//     for(let char of str ){
+//         // console.log(char)
+//         if(vowel.includes(char)){
+//             count ++;
+//         }
+//     }
+//     console.log(count)
+// }
+
+// vowelCounter('helohibyebye')
+
+
+// Problem 3: Check for Palindrome
+// Write a function that checks if a string is a palindrome (reads the same forward and backward).
+
+function palindromeChecker (palindrome){
+    const reverse= palindrome.split("").reverse().join("")
+
+    if(palindrome === reverse){
+        return true;
     }
-    console.log(count)
+    else{
+        return false;
+    }
+   
 }
 
-vowelCounter('helohibyebye')
+console.log(palindromeChecker("mam"))
 
 
